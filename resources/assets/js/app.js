2e08f8aa1915168223b426914ab690980f5c02f7
Jared Nipper <jared@vynyl.com>
2016-08-29 03:25:48 +0000

    vue, vue-resource, show list of requests

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,20 +1,10 @@
+var Vue = require('vue');
+Vue.use(require('vue-resource'));
+Vue.http.headers.common['Authorization'] = 'Bearer 268a3ef2-6c97-11e6-a18e-08002746f3a1';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * include Vue and Vue Resource. This gives a great starting point for
- * building robust, powerful web applications using Vue and Laravel.
- */
+import BeerRequests from './components/BeerRequests.vue';
 
-require('./bootstrap');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the body of the page. From here, you may begin adding components to
- * the application, or feel free to tweak this setup for your needs.
- */
-
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: 'body'
+new Vue({
+    el: '#app',
+    components: { BeerRequests },
 });
---
+++ resources/assets/js/components/BeerRequests.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <li v-for="request in requests">
+        {{ request.beer_name }}
+    </li>
+
+</template>
+
+<script>
+    export default {
+
+        created() {
+            this.fetchBeerRequests();
+        },
+
+        data() {
+            return {
+                requests: []
+            };
+        },
+
+        methods: {
+            fetchBeerRequests: function() {
+                this.$http.get('api/v1/requests').then((requests) => {
+                    this.requests = requests.data;
+                }, (error) => {
+                    console.log(error);
+                });
+            }
+        }
+
+    }
+</script>
